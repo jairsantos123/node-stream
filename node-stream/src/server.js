11083d25fd1ab7132e73d0ae6e9bdc710c117aa3
Jairo Rocha <jairabvl@hotmail.com>
2025-05-24 15:27:22 -0300

    att server

--- node-stream/src/server.js
+++ node-stream/src/server.js
@@ -1,9 +1,10 @@
 import http from 'node:http'
 
 const users = []
+let nextId = 1
 
-const server = http.createServer( async (req, res) => {
-    const {method, url} = req
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
 
     const buffers = []
 
@@ -12,31 +13,28 @@ const server = http.createServer( async (req, res) => {
     }
 
     try {
-
         req.body = JSON.parse(Buffer.concat(buffers).toString())
     } catch {
         req.body = null
-
     }
-    console.log(body)
 
     if (method === "GET" && url === '/users') {
         return res
-        .setHeader('Content-Type', 'application/json')
-        .end(JSON.stringify(users))
+            .setHeader('Content-Type', 'application/json')
+            .end(JSON.stringify(users))
     }
-    
+
     if (method === "POST" && url === '/users') {
-        const { name, email } = req.body
-        
+        const { name, email, endereco } = req.body
+
         users.push({
-            id: 1,
+            id: nextId++,
             name,
             email,
+            endereco,
         })
 
         return res.writeHead(201).end()
-
     }
 
     return res.writeHead(404).end()
